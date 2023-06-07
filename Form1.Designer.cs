4c001dd3b48ad0ee7a1b2df1e436592d6f75016d
iulianipro <59797376+iulianipro@users.noreply.github.com>
2023-06-07 22:33:22 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace XMLValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.xmltextbox = new System.Windows.Forms.TextBox();
+            this.jsontextbox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TopPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(60)))));
+            this.TopPanel.Controls.Add(this.label1);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(419, 53);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(260, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 10, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10, 5, 0, 0);
+            this.label1.Size = new System.Drawing.Size(159, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Validator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(12, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select JSON file...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select XML file...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // xmltextbox
+            // 
+            this.xmltextbox.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.xmltextbox.Location = new System.Drawing.Point(187, 77);
+            this.xmltextbox.Multiline = true;
+            this.xmltextbox.Name = "xmltextbox";
+            this.xmltextbox.ReadOnly = true;
+            this.xmltextbox.Size = new System.Drawing.Size(213, 47);
+            this.xmltextbox.TabIndex = 2;
+            this.xmltextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // jsontextbox
+            // 
+            this.jsontextbox.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.jsontextbox.Location = new System.Drawing.Point(187, 188);
+            this.jsontextbox.Multiline = true;
+            this.jsontextbox.Name = "jsontextbox";
+            this.jsontextbox.Size = new System.Drawing.Size(213, 47);
+            this.jsontextbox.TabIndex = 3;
+            this.jsontextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 360);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(146, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2, 49);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 454);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.jsontextbox);
+            this.Controls.Add(this.xmltextbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox xmltextbox;
+        private System.Windows.Forms.TextBox jsontextbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XMLValidator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string GetFilePath(object sender, EventArgs e, string file_extension)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+              //  MessageBox.Show(file_extension);
+                openFileDialog.Filter = file_extension + " files (" + file_extension + ")|" + file_extension;
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+         //   MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            return filePath;
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string file_extension = "*.xml";
+
+            xmltextbox.Text = GetFilePath(sender, e, file_extension);
+            if (XML_Validator(xmltextbox.Text)) { 
+                label2.Text= "VALID";
+                label2.ForeColor = Color.Green;
+            } else
+            {
+                label2.Text = "INVALID";
+                label2.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file_extension = "*.json";
+            jsontextbox.Text = GetFilePath(sender, e, file_extension);
+
+        }
+        private Boolean XML_Validator(String filenamepath)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+
+            string schemaFile = "Test.xsd";
+
+            XmlTextReader schemaReader = new XmlTextReader(schemaFile);
+            XmlSchema schema = XmlSchema.Read(schemaReader, SchemaValidationHandler);
+
+            xmlDoc.Schemas.Add(schema);
+            
+            xmlDoc.Load(filenamepath);
+            xmlDoc.Validate(DocumentValidationHandler);
+            
+            }
+            catch (SystemException  e ){
+               // MessageBox.Show(e.Message.ToString());
+                MessageBox.Show(e.Message.ToString(), "Error: Parse 001X1816562C", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false; 
+            }
+            return true;
+        }
+        private static void SchemaValidationHandler(object sender, ValidationEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
+        private static void DocumentValidationHandler(object sender, ValidationEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+}
+
